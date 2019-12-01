c7136abd898e5a72b1b4d78956abd6ffcde51aca
Kleberus <kleber_lsgermano@hotmail.com>
2019-12-01 03:55:17 -0300

    get Customers from backend configured

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -1,5 +1,22 @@
 import React, { Component } from "react";
 class Customers extends Component {
+  constructor() {
+    super();
+    this.state = {
+      customers: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/api/customers")
+      .then(res => res.json())
+      .then(customers =>
+        this.setState({ customers }, () =>
+          console.log("Customer fetched...", customers)
+        )
+      );
+  }
+
   render() {
     return (
       <div>
